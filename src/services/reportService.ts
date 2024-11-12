0d378845898f55701deabc1739ccd3ac9ce22882
Ivan Antonov <ivan.antonov.pro@gmail.com>
2024-11-12 16:29:44 +0700

    t

--- src/services/reportService.ts
+++ src/services/reportService.ts
@@ -447,22 +447,22 @@ export class ReportService {
   async sendPdfToTelegram(chat_id: number, pdfBuffer: Buffer): Promise<void> {
     const telegramApiUrl = `https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendDocument`;
   
-    console.log(JSON.stringify(pdfBuffer))
-
-    // const formData = new FormData();
-    // formData.append('chat_id', chat_id.toString());
-    // formData.append('document', new Blob([pdfBuffer], { type: 'application/pdf' }), 'report.pdf');
+    console.log(pdfBuffer.toString('hex'));
   
-    // try {
-    //   await axios.post(telegramApiUrl, formData, {
-    //     headers: {
-    //       ...formData.getHeaders(),
-    //     },
-    //   });
-    //   console.log(`PDF sent to chat_id: ${chat_id}`);
-    // } catch (error) {
-    //   console.error(`Failed to send PDF to chat_id: ${chat_id}`, error);
-    // }
+    const formData = new FormData();
+    formData.append('chat_id', chat_id.toString());
+    formData.append('document', new Blob([pdfBuffer], { type: 'application/pdf' }), 'report.pdf');
+  
+    try {
+      const response = await axios.post(telegramApiUrl, formData, {
+        headers: {
+          ...formData.getHeaders(),
+        },
+      });
+      console.log(`PDF sent to chat_id: ${chat_id}`, response.data);
+    } catch (error) {
+      console.error(`Failed to send PDF to chat_id: ${chat_id}`, error);
+    }
   }
 
   async sendPhoto(chat_id: number, image: any): Promise<void> {    
