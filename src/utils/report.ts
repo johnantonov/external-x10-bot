7e7448c40859a0a829949d3994064af22336c54e
Ivan Antonov <ivan.antonov.pro@gmail.com>
2024-11-13 00:50:19 +0700

    t

--- src/utils/report.ts
+++ src/utils/report.ts
@@ -21,7 +21,7 @@ export async function getReportHtml(articleData: Article[]) {
       const imgUrl = getWbArticlePhoto(+data.article);
       const response = await axios.get(imgUrl, { responseType: 'arraybuffer' });
       let imgBuffer = Buffer.from(response.data, 'binary');
-      imgBuffer = await sharp(imgBuffer).resize({ width: 80, height: 250 }).toBuffer();
+      imgBuffer = await sharp(imgBuffer).resize({ width: 120, height: 180 }).toBuffer();
       const imgBase64 = imgBuffer.toString('base64');
       imgSrc = `data:image/jpeg;base64,${imgBase64}`;
     } catch (error) {
