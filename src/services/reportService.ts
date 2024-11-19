22311b9195d1d4261764224052f84eb3047a03d3
Ivan Antonov <ivan.antonov.pro@gmail.com>
2024-11-19 16:25:59 +0700

    sales

--- src/services/reportService.ts
+++ src/services/reportService.ts
@@ -268,8 +268,8 @@ export class ReportService {
             result[el.nmID].order_info[day.dt] = {
               ordersCount: day.ordersCount,
               ordersSum: day.ordersSumRub,
-              buysCount: Math.round(day.ordersCount * ((buyoutsPercent[el.nmID] || 0) / 100)),
-              buysSum: Math.round(day.ordersSumRub * ((buyoutsPercent[el.nmID] || 0) / 100)),
+              buysCount: day.ordersCount * ((buyoutsPercent[el.nmID] || 0) / 100),
+              buysSum: day.ordersSumRub * ((buyoutsPercent[el.nmID] || 0) / 100),
               addToCartCount: day.addToCartCount,
             }
           })
