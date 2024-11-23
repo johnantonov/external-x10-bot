c389e7769f59eb900dc6e78212aacb699f8d77d7
Ivan Antonov <ivan.antonov.pro@gmail.com>
2024-11-23 17:14:06 +0700

    commit

--- src/components/botAnswers.ts
+++ src/components/botAnswers.ts
@@ -35,6 +35,7 @@ export async function handleStartMenu(msg: UserMsg | UserCallback, command: '/me
     if (isUser) {
       const type = user.type
       const text = type === 'waitTax' ? texts.updateTax : texts.menu
+      await RediceService.setUserState(chat_id, rStates.waitTax)
 
       if (!isNewMsg) {
         if (!menuId) {
