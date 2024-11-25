5130973df533b78f6664b43f8540f9d93afae463
Ivan Antonov <ivan.antonov.pro@gmail.com>
2024-11-25 15:22:57 +0700

    m

--- src/report/CSS.ts
+++ src/report/CSS.ts
@@ -55,7 +55,7 @@ export const CSS = (cols: number) => `
     td {
       white-space: nowrap;
       height: ${rowHeight};
-      width: ${mainWidth / 15}
+      width: ${mainWidth / 15} !important
     }
     th, td {
       padding: ${usualPadding}px;
