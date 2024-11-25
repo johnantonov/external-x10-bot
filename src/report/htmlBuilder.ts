fa7d6cd24767344fbafcfee53362f3e904b2920f
Ivan Antonov <ivan.antonov.pro@gmail.com>
2024-11-25 12:30:20 +0700

    m

--- src/report/htmlBuilder.ts
+++ src/report/htmlBuilder.ts
@@ -32,7 +32,7 @@ export const generateDayRows = (data: SKU, imgSrc: string | null, days: `${numbe
   const dayCount = config.pdf.tableDays
 
   const total: { [key: string]: number[] | number} = {};
-  let totalRow = `<td rowspan="1" class = "total_row" colspan="${config.pdf.dayColspan}">Итог</td>`;
+  let totalRow = `<td rowspan="1" class="total_row" colspan="${config.pdf.dayColspan}">Итог</td>`;
 
   const titleCol = `
     <td rowspan="${dayCount+1}" colspan="${config.pdf.photoColspan}">
