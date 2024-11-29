0e989dfcd154de3709f1757b42a171fc4d5b6ad6
Ivan Antonov <ivan.antonov.pro@gmail.com>
2024-11-29 21:25:18 +0700

    m

--- src/utils/requestReport.ts
+++ src/utils/requestReport.ts
@@ -5,10 +5,10 @@ dotenv.config();
 
 export async function requestReport(chat_id: string | number) {
   try {
-    const response = await axios.post(`http://localhost:${process.env.BASE_PORT}/generate-report`, { chat_id });
+    const response = await axios.post(`http://localhost:${process.env.BASE_PORT}/generate-report`, { chat_id: chat_id });
     return response.data;
   } catch (error) {
     console.error("Error сalling ReportService:", error);
-    throw error;
+    return null
   }
 }
