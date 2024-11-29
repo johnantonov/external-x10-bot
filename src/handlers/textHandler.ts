e5906de0bd4e68d994ae8326ce63b25fe5630a6f
Ivan Antonov <ivan.antonov.pro@gmail.com>
2024-11-29 18:37:48 +0700

    m

--- src/handlers/textHandler.ts
+++ src/handlers/textHandler.ts
@@ -26,7 +26,10 @@ export async function handleUserState(chat_id: number, msgs: MessageMS[], userTe
 
     if (!answer.result) {
       await MS.saveMessages(msgs);
-      return MS.editMessage(chat_id, response.message_id, answer.text, mainOptions(answer?.type)!)
+      if (answer?.type) {
+        return MS.editMessage(chat_id, response.message_id, answer.text, mainOptions(answer.type)!)
+      }
+      return bot.editMessageText(answer.text, { chat_id, message_id: response.message_id });
     } else {
       await RediceService.deleteUserState(chat_id);
       await MS.deleteOldAndNewMessages(chat_id, msgs);
