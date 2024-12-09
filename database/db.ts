d38c3fd45d63c20e37a837efcb62f1548e5aa2d4
Ivan Antonov <ivan.antonov.pro@gmail.com>
2024-12-09 17:23:50 +0700

    deploy prod

--- database/db.ts
+++ database/db.ts
@@ -3,7 +3,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const pool = new Pool({
-  host: 'external-x10Bot-postgres',
+  host: process.env.PGHOST,
   port: Number(process.env.PGPORT),
   user: process.env.PGUSER,
   password: process.env.PGPASS,
