e0e4c583e26d9a94bd9f303cf3df682f0b6f939d
Ivan Antonov <ivan.antonov.pro@gmail.com>
2024-12-11 21:37:45 +0700

    ref

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -128,7 +128,7 @@ export function formatDateForTimestamp(date: Date): string {
   const year = date.getFullYear();
   const month = pad(date.getMonth() + 1); 
   const day = pad(date.getDate());
-  const hours = pad(date.getHours()) + 3;
+  const hours = pad(date.getHours() + 3);
   const minutes = pad(date.getMinutes());
   const seconds = pad(date.getSeconds());
   const milliseconds = date.getMilliseconds().toString().padStart(3, '0'); 
