a5e259d4c6ee57d25681af911b5fc7fb53e43315
Ivan Antonov <ivan.antonov.pro@gmail.com>
2024-12-11 17:58:19 +0700

    stats

--- src/services/botStatsService.ts
+++ src/services/botStatsService.ts
@@ -8,17 +8,22 @@ dotenv.config()
 const env = process.env
 
 export async function sendBotStats() {
-  if (env.MODE === 'test') {
-    const statsData: BotStatsPayload = {
-      action: 'base_stats',
-      data: [
-        (await users_db.getAllUsers())?.length || 0,  
-        (await users_db.getReportUsers())?.length || 0
-      ]
-    };
-
-    const response = await axios.post(env.STATS_WEB_APP_URL!, statsData, { headers: { 'Content-Type': 'application/json' }})
-    console.log('Response from stats web app: ', JSON.stringify(response))
+  try {
+    if (env.MODE === 'test') {
+      const statsData: BotStatsPayload = {
+        action: 'base_stats',
+        data: [
+          (await users_db.getAllUsers())?.length || 0,  
+          (await users_db.getReportUsers())?.length || 0
+        ]
+      };
+  
+      const response = await axios.post(env.STATS_WEB_APP_URL!, statsData, { headers: { 'Content-Type': 'application/json' }})
+      const res = response.data
+      console.log('Response from stats web app: ', JSON.stringify(res))
+    }
+  } catch (e) {
+    console.error('Error sending bot base stats')
   }
 }
 
@@ -26,3 +31,5 @@ cron.schedule('30 * * * *', async () => {
   console.log('Running bot stats service at:', new Date().toLocaleTimeString('ru-RU', { timeZone: 'Europe/Moscow' }));
   await sendBotStats();
 }, { timezone: 'Europe/Moscow' });
+
+console.log('Stats Service started!')
