41e51691bf38208c271f40b51d9251f68618a2a5
Ivan Antonov <ivan.antonov.pro@gmail.com>
2024-12-13 14:57:17 +0700

    sales report

--- src/handlers/callbackHandler.ts
+++ src/handlers/callbackHandler.ts
@@ -4,7 +4,7 @@ import { redis, rStates, ttls } from "../redis";
 import { handleStartMenu } from "../components/botAnswers";
 import { RediceService } from "../bot";
 import { createEditData, MessageService } from "../services/messageManipulatorService";
-import { articleOptions, CallbackData, generateArticlesButtons, generateReportTimeButtons, mainOptions, Options, ordersReportMenu, returnMenu, yesNo } from "../components/buttons";
+import { articleOptions, CallbackData, generateArticlesButtons, generateReportTimeButtons, mainOptions, Options, ordersReportMenu, returnMenu, salesReportMenu, yesNo } from "../components/buttons";
 import { users_db } from "../../database/models/users";
 import { getCurrentArticle, parseArticleData } from "../utils/parse";
 import { articles_db } from "../../database/models/articles";
@@ -257,7 +257,8 @@ export async function callbackHandler(query: TelegramBot.CallbackQuery, bot: Tel
         console.log('reportType: '+reportType)
 
         if (!ordersCallback[1]) {
-          editData = createEditData(" ", ordersReportMenu());
+          const reportMenu = reportType === 'orders' ? ordersReportMenu() : salesReportMenu()
+          editData = createEditData(" ", reportMenu);
         } else if (ordersCallback[1] === 'today' || ordersCallback[1] === 'yesterday') {
           MS.deleteAllMessages(chat_id);
           const loadingMsg = await bot.sendMessage(chat_id, texts.loadingReports, { disable_notification: true });
